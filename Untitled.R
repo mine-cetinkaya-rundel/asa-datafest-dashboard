8d49469ae806a0e39c116c01c0acf580da8de136
Jenyy0416 <106097410+Jenyy0416@users.noreply.github.com>
2022-06-24 10:11:13 -0400

    Revert "Slides"

--- Untitled.R
+++
@@ -1,3 +0,0 @@
-updated_datafest <- read.csv("data/updated_datafest.csv")
-updated_datafest <- updated_datafest %>% rename(BestVisualization = winners_visualization)
-updated_datafest <- updated_datafest %>% mutate(JudgesChoice = "", .after = BestUseOfExternalData)
